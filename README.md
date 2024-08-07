8931b931f77174119e10abc27244f598c737bcc1
Gabriel Torreiro <glopestorreiro@gmail.com>
2024-08-07 13:32:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Projeto: Aplicativo de Lista de Tarefas (To-Do List)
+
+## Descrição
+Um aplicativo web simples, dividido em cliente e servidor, para gerenciar tarefas diárias, permitindo aos usuários adicionar, editar, excluir e marcar tarefas como concluídas.
+
+## Links dos Repositórios
+[Servidor](https://github.com/gabrieltorreiro/todo-list-api)
+
+[Cliente](https://github.com/gabrieltorreiro/todo-list-client) (Este Repositório)
+
+## Requisitos
+
+ - Node v18.20.1
+ - NPM v9.2.0
+
+## Como rodar?
+
+```sh
+git clone https://github.com/gabrieltorreiro/todo-list-client
+cd todo-list-client
+npm run start:dev
+```
+
+## Funcionalidades Essenciais
+
+### Usuario
+- [ ] Criar uma conta
+- [ ] Logar na conta
+- [ ] Recuperar senha
+- [ ] Excluir conta
+- [ ] Editar dados
+
+### Lista de Tarefas
+- [ ] Criar Lista de Tarefas
+- [ ] Editar Lista de Tarefas
+- [ ] Deletar Lista de Tarefas
+- [ ] Filtrar listas por nome
+
+### Tarefas
+- [X] Buscar tarefa
+- [X] Criar Tarefa
+- [X] Editar tarefa
+- [X] Deletar tarefa
+- [x] Marcar tarefa como concluída
+- [ ] Filtrar tarefas por nome
+
+## Funcionalidades Extras
+
+### Tarefas
+- [ ] Adicionar prazos para tarefas
+- [ ] Notificações por email ou push
+- [ ] Categorizar tarefas por tags
+
+## Escopo Inicial
+A versão inicial do aplicativo incluirá todas as funcionalidades essenciais. Funcionalidades extras podem ser adicionadas em futuras atualizações.
---
+++ src/app/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Task {
+  id: number;
+  name: string;
+  status: boolean;
+}
+
+const TasksPage = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [newTask, setNewTask] = useState<string>("");
+
+  const baseURL = "http://localhost:5000";
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get(`${baseURL}/task`);
+        setTasks(response.data);
+      } catch (err) {
+        setError("Erro ao buscar tarefas");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const handleAddTask = async () => {
+    if (!newTask.trim()) return;
+    try {
+      const response = await axios.post(`${baseURL}/task`, { name: newTask });
+      setTasks([...tasks, response.data]);
+      setNewTask("");
+    } catch (err) {
+      setError("Erro ao adicionar tarefa");
+    }
+  };
+
+  const handleDeleteTask = async (id: number) => {
+    try {
+      await axios.delete(`${baseURL}/task/${id}`);
+      setTasks(tasks.filter((task) => task.id !== id));
+    } catch (err) {
+      setError("Erro ao deletar tarefa");
+    }
+  };
+
+  if (loading) return <p className="text-center">Loading...</p>;
+  if (error) return <p className="text-center text-red-500">{error}</p>;
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center w-11/12 mx-auto">
+      <div className="flex flex-col items-center p-4 w-full">
+        <button
+          onClick={() => setIsEditing(!isEditing)}
+          className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ml-auto"
+        >
+          {isEditing ? "Concluir" : "Editar"}
+        </button>
+        <h1 className="text-3xl font-bold mb-6">Tarefas</h1>
+        <ul className="w-full max-w-md shadow-md rounded-lg p-4">
+          {tasks.map((task) => (
+            <li
+              key={task.id}
+              className={`flex justify-between items-center p-2 ${
+                task.status ? "text-green-600 line-through" : "text-white-700"
+              } capitalize`}
+            >
+              <span>{task.name}</span>
+
+              <button
+                onClick={() => handleDeleteTask(task.id)}
+                className={`ml-4 px-2 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-600 ${
+                  isEditing ? "visible" : "invisible"
+                }`}
+              >
+                Excluir
+              </button>
+            </li>
+          ))}
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className={`mt-3 p-2 border border-gray-300 rounded-lg bg-transparent w-full ${
+              isAdding ? "visible" : "invisible"
+            }`}
+            placeholder="Digite nome da tarefa"
+          />
+        </ul>
+      </div>
+      <div className="flex justify-center mb-auto w-full">
+      <button
+        onClick={() => {
+          setIsAdding(!isAdding);
+          if (isAdding) handleAddTask();
+        }}
+        className="px-4 py-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 w-11/12"
+      >
+        {isAdding ? "Concluir" : "Adicionar"}
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default TasksPage;
